b5993f46a49e2c62b2aa09a49eb5b91492a61d12
Morimel <melsovisa77@gmail.com>
2025-06-05 16:35:45 +0600

    Initial Commit

---
+++ MonySwift/MonySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonySwiftApp.swift
+//  MonySwift
+//
+//  Created by Isa Melsov on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MonySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
